a48041ca59cd24a6bdf8aed039f04fa2e932e880
Tetsuo Aoyagi <aoyagi@opentone.co.jp>
2016-08-04 18:52:18 +0900

    BSOL_LB-46 【実装】全文検索インデックス作成 - 画像からトークンを抽出しインデックスに登録する
    - 空ソースのみ。中身はこれから

---
+++ linkbinder-framework-core/src/test/java/jp/co/opentone/bsol/framework/core/google/vision/GoogleVisionApiClientTest.java
@@ -0,0 +1,11 @@
+package jp.co.opentone.bsol.framework.core.google.vision;
+
+/**
+ * Created by t-aoyagi on 2016/08/04.
+ */
+public class GoogleVisionApiClientTest {
+
+    public void testTextDetection() throws Exception {
+
+    }
+}
